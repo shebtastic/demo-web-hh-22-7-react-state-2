5da21ba45c9583a0f162389b8636a344f6a287e1
Sheb <michael@sheb.rocks>
2022-12-16 10:27:17 +0100

    demo version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import SearchForm from "./components/SearchForm";
 import SearchResults from "./components/SearchResults";
 import "./styles.css";
 
 export default function App() {
+  const [formData, setFormData] = useState()
+
+  console.log(formData)
+
   return (
     <main>
       <h1>Search</h1>
-      <SearchForm />
-      <SearchResults />
+      <SearchForm onSubmit={setFormData} />
+      <SearchResults searchTerm={formData?.searchTerm} />
     </main>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,36 @@
-export default function SearchForm() {
-  const searchTerm = "";
+// import { useState } from "react";
+
+export default function SearchForm({onSubmit}) {
+  // const [searchTerm, setSearchTerm] = useState("")
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log("submit");
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData)
+
+    onSubmit(data)
+    // onSubmit({searchTerm})
   }
 
+  // ternary operator = <bedingung> ? <if true wert> : <else wert>
+  // optional chaining = <potentiell null oder undefined object>?.<property key>
+  // nullish coalescence = <null oder undefined wert> ?? <stattdessen dieser default wert>
+  // short circuit OR = <wert ist falsy> || <stattdessen dieser default wert>
+  // short circuit AND = <wert ist truthy> && <nur dann wird der wert hier angeschaut>
+  //                     <wert ist truthy> ? <nur dann wird der wert hier angeschaut> : undefined
+  
   return (
     <>
+      {/* form.addEventListener("submit", handleSubmit) */}
       <form onSubmit={handleSubmit}>
         <label htmlFor="searchTerm">Search term:</label>
-        <input name="searchTerm" id="searchTerm" />
+        <input
+          // onChange={(event) => setSearchTerm(event.target.value)}
+          // value={searchTerm}
+          name="searchTerm"
+          id="searchTerm"
+        />
         <button>
           <span role="img" aria-label="search icon">
             🔍
@@ -19,11 +38,11 @@ export default function SearchForm() {
           Search
         </button>
       </form>
-      <h2>
+      {/* <h2>
         {searchTerm
           ? `You searched for "${searchTerm}"`
           : "Please enter a search term"}
-      </h2>
+      </h2> */}
     </>
   );
 }
